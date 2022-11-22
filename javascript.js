9c92360620142887f4046e86dfc22f49febd25a2
monkeydesu <monkeydesu@gmail.com>
2022-11-22 15:55:58 +1300

    Create playerChoice function

--- javascript.js
+++ javascript.js
@@ -10,4 +10,21 @@ function getComputerChoice() {
     return console.log(x)
 }
 
+
+function playerChoice() {
+    isValid = false;
+    while (isValid === false) {
+        let y = prompt("Please enter rock, paper, or scissors: ");
+        if (y === "rock" || y === "paper" || y === "scissors") {
+            isValid = true
+            return console.log(y)
+        }
+        else {
+            console.log("Please enter a valid answer.")
+        }
+    }
+}
+
+
 getComputerChoice()
+playerChoice()
